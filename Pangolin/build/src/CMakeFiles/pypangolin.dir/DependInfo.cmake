8c7caa589e7d03ec276d7eb988fa647ef1140b84
Saksham Khatod <saksham.khatod92@gmail.com>
2020-12-01 21:18:09 +0530

    Updated README

---
+++ Pangolin/build/src/CMakeFiles/pypangolin.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/attach.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/attach.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/colour.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/colour.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/datalog.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/datalog.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/display.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/display.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/gl.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/gl.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/gl_draw.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/gl_draw.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/glsl.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/glsl.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/handler.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/handler.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/image.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/image.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/image_view.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/image_view.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/opengl_render_state.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/opengl_render_state.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/params.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/params.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/pixel_format.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/pixel_format.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/plotter.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/plotter.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/var.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/var.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/video.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/video.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/view.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/view.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/viewport.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/viewport.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/widget.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/widget.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin/window.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin/window.cpp.o"
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/src/python/pypangolin_module.cpp" "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pypangolin.dir/python/pypangolin_module.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "pypangolin_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/pybind11/include"
+  "/Users/sakshamramkhatod/opt/anaconda3/include/python3.7m"
+  "../include"
+  "src/include"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  "/usr/local/include"
+  "/usr/local/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/sakshamramkhatod/Desktop/Stuff/slam/Pangolin/build/src/CMakeFiles/pangolin.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # slam
+This project requires pygame, OpenGL & Pangolin to work.
+I'm including a modified version of Pangolin into the repository which has the relevant python bindings.
